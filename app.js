d7934b1495e572fdee3f4545378a611f54c6833b
SuperCoder <propscode254@gmail.com>
2023-12-08 10:50:50 +0100

    first

---
+++ app.js
@@ -0,0 +1,15 @@
+import "dotenv/config"; //hier gibt es schon process.env.
+process.env.TEUER_KEY = "11111111";
+// Liest die dateien und schreibt es unseren process.env dazu
+// ANdere version:
+// import dotenv from "dotenv";
+import express from "express";
+//Zugriff auf UMgebungsvariablen von Betriebsystemnode
+
+console.log("-------✅", process.env.BACKEND_URL);
+console.log("-------🌸", process.env.MONGODB);
+console.log("-------🍺", process.env.BANK_API_KEY);
+
+const app = express();
+
+app.listen(8000);
